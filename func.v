1b934c11adfdc701a7e82a9aaecd9599b6ca59f8
denismedvedev478 <denismedvedev478@gmail.com>
2025-05-07 02:14:52 +0400

    Working pipeline + 2 tests

--- func.v
+++ func.v
@@ -28,51 +28,121 @@ module funcQ #(
 reg signed [2*DATA_WIDTH-1:0] tmp1;
 reg signed [2*DATA_WIDTH-1:0] tmp2;
 reg signed [2*DATA_WIDTH-1:0] tmp3;
-reg signed [2*DATA_WIDTH-1:0] result;
+reg signed [2*DATA_WIDTH-1:0] tmp4;
 
+
+reg signed [DATA_WIDTH-1:0] a_r;
+reg signed [DATA_WIDTH-1:0] b_r;
+reg signed [DATA_WIDTH-1:0] c_r;
 
 reg signed [DATA_WIDTH-1:0] d_r;
 reg signed [DATA_WIDTH-1:0] d_r2;
+reg signed [DATA_WIDTH-1:0] d_r3;
 
 reg vld_r;
 reg vld_r2;
 reg vld_r3;
+reg vld_r4;
 
 always_ff @(posedge clk or posedge rst) begin
 	if (rst) begin
 		tmp1 <= 0;
 		tmp2 <= 0;
 		tmp3 <= 0;
-		result <= 0;
+		tmp4 <= 0;
 		Q <= 0;
-		d_r <=0;
-		d_r2 <= 0;
 		Q_vld <= 0;
+		
+		a_r <= 0;
+		b_r <= 0;
+		c_r <= 0;
+		d_r <= 0;
+		d_r2 <= 0;
+		
 		vld_r <= 0;
 		vld_r2 <= 0;
 		vld_r3 <= 0;
-	end 
-	else begin
+	end
+	else begin	// more energy efficient but more area
 		if (data_vld) begin
-			tmp1 <= a - b;
-			tmp2 <= 1 + 3 * c;
+			a_r <= a;
+			b_r <= b;
+			c_r <= c;
 			d_r <= d;
+		end
+		vld_r <= data_vld;
+		
+		if (vld_r) begin
+			tmp1 <= a_r - b_r;
+			tmp2 <= 1 + 3 * c_r;
 			d_r2 <= d_r;
 		end
-		
-		// vld propagation
-		vld_r <= data_vld;
 		vld_r2 <= vld_r;
+		
+		if (vld_r2) begin
+			tmp3 <= tmp1 * tmp2;
+			d_r3 <= d_r2;
+		end
 		vld_r3 <= vld_r2;
 		
-		tmp3 <= tmp1 * tmp2;
-		result <= tmp3 - (4 * d_r2);
-		Q <= result >>> 1;
-		Q_vld <= vld_r3;
+		if (vld_r3) begin
+			tmp4 <= tmp3 - (4 * d_r3);
+		end
+		vld_r4 <= vld_r3;
+		
+		if (vld_r4) begin
+			Q <= tmp4 >>> 1;
+			Q_vld <= 1;
+		end else begin
+			Q_vld <= 0;
+		end
+
 	end
 end
 
-
-
+/*
+always_ff @(posedge clk or posedge rst) begin	// less area and less energy efficient
+	if (rst) begin
+		tmp1 <= 0;
+		tmp2 <= 0;
+		tmp3 <= 0;
+		tmp4 <= 0;
+		Q <= 0;
+		Q_vld <= 0;
+		
+		a_r <= 0;
+		b_r <= 0;
+		c_r <= 0;
+		d_r <= 0;
+		d_r2 <= 0;
+		
+		vld_r <= 0;
+		vld_r2 <= 0;
+		vld_r3 <= 0;
+	end
+	else begin
+		a_r <= a;
+		b_r <= b;
+		c_r <= c;
+		d_r <= d;
+		vld_r <= data_vld;
+		
+		tmp1 <= a_r - b_r;
+		tmp2 <= 1 + 3 * c_r;
+		d_r2 <= d_r;
+		vld_r2 <= vld_r;
+		
+		tmp3 <= tmp1 * tmp2;
+		d_r3 <= d_r2;
+		vld_r3 <= vld_r2;
+		
+		tmp4 <= tmp3 - (4 * d_r3);
+		vld_r4 <= vld_r3;
+		
+		Q <= tmp4 >>> 1;
+		Q_vld <= vld_r4;
+	end
+end
+*/
 
 endmodule
--- testbench.sv
+++ testbench.sv
@@ -17,13 +17,15 @@ logic signed [DATA_WIDTH-1:0] Q;
 
 funcQ #(.DATA_WIDTH(DATA_WIDTH)) DUT (.* );
 
-initial clk = 0;
-always #5 clk = ~clk;
+initial clk <= 0;
+always #5 clk <= ~clk;
 
 initial begin
-rst = 1;
-repeat (2) @(posedge clk);
-rst = 0;
+	rst <= 1;
+	repeat (10) begin
+		@(posedge clk)
+		rst <= 0;
+	end
 end
 
 initial begin
@@ -36,21 +38,63 @@ initial begin
 end
 
 
+// hardcoded tests
+reg signed [DATA_WIDTH-1:0] a1 = 11, b1 = 5, c1 = -1, d1 = 5;
+
+reg signed [DATA_WIDTH-1:0] a2 = 12, b2 = 3, c2 = -2, d2 = 1;
+	
+	
 initial begin
-	@(negedge rst);
+    repeat (4) begin
+        @(posedge clk);
+        data_vld <= 0;
+        a <= 0; b <= 0; c <= 0; d <= 0;
+    end
 
-	a = 12; b = 5; c = -2; d = 3;
-	data_vld = 1;
-	@(posedge clk);
-	data_vld = 0;
+    @(posedge clk);
+    data_vld <= 1;
+    a <= 11; b <= 5; c <= -1; d <= 5;
+    //signed [DATA_WIDTH-1:0] a1 = 11, b1 = 5, c1 = -1, d1 = 5;
 
-	wait (Q_vld) begin
-		if (Q !== ref_funcQ(a, b, c, d))
-		  $display("FAIL: expected %0d, got %0d", ref_funcQ(a, b, c, d), Q);
-		else
-		  $display("PASS: Q = %0d", Q);
-	end
-	$finish;
+    @(posedge clk);
+    a <= 12; b <= 3; c <= -2; d <= 1;
+    //signed [DATA_WIDTH-1:0] a2 = 12, b2 = 3, c2 = -2, d2 = 1;
+
+    @(posedge clk);
+    data_vld <= 0;
+    a <= 0; b <= 0; c <= 0; d <= 0;
 end
+
+initial begin
+    check_result(a1, b1, c1, d1);
+    check_result(a2, b2, c2, d2);
+
+    $finish;
+end
+
+
+task automatic check_result;
+    input signed [DATA_WIDTH-1:0] a_in;
+    input signed [DATA_WIDTH-1:0] b_in;
+    input signed [DATA_WIDTH-1:0] c_in;
+    input signed [DATA_WIDTH-1:0] d_in;
+
+    reg signed [DATA_WIDTH-1:0] expected;
+    begin
+        expected = ref_funcQ(a_in, b_in, c_in, d_in);
+
+        @(posedge clk);
+        wait (Q_vld);
+
+        if (Q !== expected)
+            $display("FAIL: expected %0d, got %0d", expected, Q);
+        else
+            $display("PASS: Q = %0d", Q);
+
+        @(posedge clk);
+        wait (!Q_vld);
+    end
+endtask
+
 
 endmodule
